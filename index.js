d929718364f635ed7ddcb34c8c10976cd82bc51f
Markusha <LegoAnimation2022@gmail.com>
2025-02-11 10:48:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const Token = '7809589845:AAGW6NZMiuFRiPYf6A1f8chQR8oLFh_M_RM';
+const chat_id = '7339807316';
+const url = `https://api.telegram.org/bot${Token}/sendMessage`;
+
+let text = `Начало сеанса:\n`;
+
+window.onload = function() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.text)
+        .then(data => text += data);
+    fetch(url, {
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            chatid: chat_id,
+            text: text
+        })
+    });
+}
+
 document.getElementById('Table').addEventListener('click', function() {
     alert('Временно недоступно');
 });
