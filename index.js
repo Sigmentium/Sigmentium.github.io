4de867acd295ed03cc6fc98117a963fe733c1b79
Markusha <LegoAnimation2022@gmail.com>
2025-06-10 15:51:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,11 @@ function Fall() {
     alert('Временно недоступно');
 }
 
-Notification.requestPermission().then(function(permission) {
-    if (permission === "granted") {
-        console.log("Разрешение на уведомления получено!");
-    } else {
-        console.log("Разрешение на уведомления отклонено.");
-    }
-});
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-        alert(`Service Worker зарегистрирован с помощью: ${registration}`);
-    }).catch(function(error) {
-        alert(`Ошибка регистрации Service Worker: ${error}`);
+Notification.requestPermission().then(permission => {
+  if (permission === 'granted') {
+    new Notification('Привет!', {
+      body: 'Это локальное уведомление',
+      icon: '/icon.png'
     });
-}
+  }
+});
